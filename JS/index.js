88408457125972f2120c9f10daadf8a64e3869d5
SazidAF <Sazid@AF.gmail.com>
2021-07-06 01:51:24 +0600

    hashing attempt 3

--- JS/index.js
+++ JS/index.js
@@ -34,7 +34,7 @@ function signUp() {
           uid = user.uid;
         }
         var firebaseRef = firebase.database().ref("users/");
-        
+        this.userPassword = hashPassword(userPassword);
         var userData = {
           userFullName: userFullName,
           userEmail: userEmail,
@@ -303,4 +303,16 @@ function signOut() {
       var errorMessage = error.message;
       swal(errorMessage, "warning");
     });
+}
+
+
+/*********Hashing ***************/
+const bcrypt = require ('bcrypt');
+const saltRounds = 10;
+function hashPassword(password){
+  bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(password, salt, function(err, hash) {
+      return hash;
+    });
+  });
 }
