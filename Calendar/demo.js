82d35a2d68a8dcf21e2b8900b26fc704f0ab4abc
ntanjila <nahidaakter@iut-dhaka.edu>
2021-07-28 01:20:51 +0600

    Google Calendar event shown

--- Calendar/demo.js
+++ Calendar/demo.js
@@ -1,4 +1,159 @@
+// google api js
 
+// Client ID and API key from the Developer Console
+var CLIENT_ID =
+"778401257741-6a75o3ij228620vchcjjovh3muh3hu6s.apps.googleusercontent.com";
+var API_KEY = "AIzaSyBzFIsDwS53ueZZ9X79V06CvATUbde5XyI";
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = [
+"https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+
+var authorizeButton = document.getElementById("authorize_button");
+var signoutButton = document.getElementById("signout_button");
+
+/**
+*  On load, called to load the auth2 library and API client library.
+*/
+function handleClientLoad() {
+gapi.load("client:auth2", initClient);
+}
+
+/**
+*  Initializes the API client library and sets up sign-in state
+*  listeners.
+*/
+function initClient() {
+gapi.client
+  .init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES,
+  })
+  .then(
+    function () {
+      // Listen for sign-in state changes.
+      gapi.auth2
+        .getAuthInstance()
+        .isSignedIn.listen(updateSigninStatus);
+
+      // Handle the initial sign-in state.
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      authorizeButton.onclick = handleAuthClick;
+      signoutButton.onclick = handleSignoutClick;
+    },
+    function (error) {
+      appendPre(JSON.stringify(error, null, 2));
+    }
+  );
+}
+
+/**
+*  Called when the signed in status changes, to update the UI
+*  appropriately. After a sign-in, the API is called.
+*/
+function updateSigninStatus(isSignedIn) {
+if (isSignedIn) {
+  authorizeButton.style.display = "none";
+  signoutButton.style.display = "block";
+  listUpcomingEvents();
+} else {
+  authorizeButton.style.display = "block";
+  signoutButton.style.display = "none";
+}
+}
+
+/**
+*  Sign in the user upon button click.
+*/
+function handleAuthClick(event) {
+gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+*  Sign out the user upon button click.
+*/
+function handleSignoutClick(event) {
+gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+* Append a pre element to the body containing the given message
+* as its text node. Used to display the results of the API call.
+*
+* @param {string} message Text to be placed in pre element.
+*/
+function appendPre(message) {
+var pre = document.getElementById("content");
+var textContent = document.createTextNode(message + "\n");
+pre.appendChild(textContent);
+}
+
+var google_events = [];
+
+function listUpcomingEvents() {
+  gapi.client.calendar.events
+  .list({
+    calendarId: "primary",
+    timeMin: new Date().toISOString(),
+    showDeleted: false,
+    singleEvents: true,
+    maxResults: 10,
+    orderBy: "startTime",
+  })
+  .then(function (response) {
+    var events = response.result.items;
+    
+    //appendPre("Upcoming events:");
+
+    if (events.length > 0) {
+      
+      
+      
+      for (var i, i = 0; i < events.length; i++) {
+       
+
+        var temp_event = events[i];
+        console.log(events[i]);
+        var when = temp_event.start.dateTime;
+        if (!when) {
+          when = temp_event.start.date;
+        }
+        console.log(when);
+
+        var new_event2 = {
+              id: events[i].id,
+              name: events[i].summary,
+              description: events[i].summary,
+              date: when,
+              type: "event",
+              everyYear: !0,
+            };
+        
+        google_events.push(new_event2);
+
+            
+      }
+      console.log(google_events);
+      //appendPre(temp_event.summary + " (" + when + ")");
+    } else {
+      //appendPre("No upcoming events found.");
+    }
+ });
+
+ 
+}
+
+
+
+
+// google api js
 
 var firebaseConfig = {
   apiKey: "AIzaSyBzFIsDwS53ueZZ9X79V06CvATUbde5XyI",
@@ -90,7 +245,7 @@ $(document).ready(function () {
       $("#demoEvoCalendar").evoCalendar({
         format: "MM dd, yyyy",
         titleFormat: "MM",
-        calendarEvents: event_array,
+        calendarEvents: $.merge(event_array, google_events),
         
       }); 
       //console.log(calendarEvents);
